841193a00703609bbbf8cb1a6cd1763ef712144d
shreyarth <shbhatt@ucsc.edu>
2019-12-07 14:45:23 -0800

    Initial push, will make desgintest soon

---
+++ Assets/Scripts/Generator_Test.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator_Test : MonoBehaviour
+{
+    public List<GameObject> boxes = new List<GameObject>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject box = boxes[Random.Range(0, boxes.Count)];
+        Instantiate(box, transform.position, Quaternion.identity);
+        box.transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
+        Destroy(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<SpriteRenderer>().flipX = true;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetAxis("Horizontal") > 0f)
+        {
+            this.GetComponent<SpriteRenderer>().flipX = true;
+            transform.Translate(0.5f, 0f, 0f);
+        }
+        else if(Input.GetAxis("Horizontal") < 0f)
+        {
+            this.GetComponent<SpriteRenderer>().flipX = false;
+            transform.Translate(-0.5f, 0f, 0f);
+        }
+
+        if (Input.GetAxis("Vertical") > 0f)
+        {
+            transform.Translate(0f, 0f, 0.5f);
+        }
+        else if (Input.GetAxis("Vertical") < 0f)
+        {
+            transform.Translate(0f, 0f, -0.5f);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.Translate(0f, 0.5f, 0f);
+        }
+    }
+}
